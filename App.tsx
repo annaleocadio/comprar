ff3b38751da355ca5eace88c509e41952f5e5497
annaleocadio <annaclaraleocadio@yahoo.com>
2026-01-30 18:40:30 -0300

    ajusta App e adiciona styles

--- App.tsx
+++ App.tsx
@@ -1,20 +1,11 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { View, Text, StyleSheet } from "react-native";
+
+import { styles } from  "./styles"
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text style={styles.text}>Eu amo minha namorada!</Text>
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
